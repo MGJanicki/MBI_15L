baefc67a797a3d8ee4cc54d8508b2eea1128d64d
Marcin Janicki <mgjanicki@gmail.com>
2015-06-06 20:25:55 +0200

    Testy i interfejs dla algorytmu Smitha-Watermana

---
+++ js/SmithWaterman.js
@@ -0,0 +1,10 @@
+//funkcja zwraca tablicê obiektów z najlepszymi lokalnymi dopasowaniami
+function smithWaterman(sequence1, sequence2, scoringMatrix, gapCost)
+{
+	
+}
+
+exports.smithWaterman = function(sequence1, sequence2, scoringMatrix, gapCost)
+{
+	return smithWaterman(sequence1, sequence2, scoringMatrix, gapCost);
+}
---
+++ test/SmithWatermanTest.js
@@ -0,0 +1,47 @@
+var test = require('unit.js');
+var assert = test.assert;
+var smithWaterman = require('../js/SmithWaterman.js');
+
+describe('High scoring pairs test', function(){
+	
+	//inicjalizacja macierzy podobieñstwa wykorzystywanej do testów
+	var scoringMatrix = [];
+	for(i = 0; i < 4; i++)
+	{
+		scoringMatrix[i] = [];
+		for(j = 0; j < 4; j++)
+		{
+			if(i === j)
+			{
+				scoringMatrix[i][j] = 1;
+			}
+			else
+			{
+				scoringMatrix[i][j] = -1;
+			}
+		}
+	}
+	
+	var gapCost = -1;
+	
+	it('Smith-Waterman algorithm test', function(){
+		//test na podstawie przyk³adu ze slajdów do drugiego wyk³adu
+		var results = smithWaterman.smithWaterman('GAAAGAT', 'GATGAA', scoringMatrix, gapCost);
+		
+		test.array(results);
+		
+		assert.strictEqual(results.length, 2);
+		
+		assert.strictEqual(results[0].index, 4);
+		
+		assert.strictEqual(results[0].score, 3);
+		
+		assert.strictEqual(results[0].sequenceLength, 3);
+		
+		assert.strictEqual(results[1].index, 1);
+		
+		assert.strictEqual(results[1].score, 3);
+		
+		assert.strictEqual(results[1].sequenceLength, 3);
+	});
+});
